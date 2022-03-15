ef047337ad5daa60f98df55b14a68cb8045a928f
minamotorin <76122224+minamotorin@users.noreply.github.com>
2022-03-15 17:40:22 +0000

    Update datelock.py
    Related to https://github.com/twintproject/twint/issues/1136

--- twint/datelock.py
+++ twint/datelock.py
@@ -2,7 +2,9 @@ import datetime
 
 import logging as logme
 
-from .tweet import utc_to_local
+#from .tweet import utc_to_local
+def adjust_tz(dt):
+    return dt.astimezone(tz=datetime.timezone.utc).astimezone(tz=None)
 
 
 class Datelock:
@@ -28,17 +30,17 @@ def Set(Until, Since):
 
     if Until:
         d.until = datetime.datetime.strptime(convertToDateTime(Until), "%Y-%m-%d %H:%M:%S")
-        d.until = utc_to_local(d.until)
+        d.until = adjust_tz(d.until)
     else:
         d.until = datetime.datetime.today()
 
     if Since:
         d.since = datetime.datetime.strptime(convertToDateTime(Since), "%Y-%m-%d %H:%M:%S")
-        d.since = utc_to_local(d.since)
+        d.since = adjust_tz(d.since)
         d._since_def_user = True
     else:
         d.since = datetime.datetime.strptime("2006-03-21 00:00:00", "%Y-%m-%d %H:%M:%S")
-        d.since = utc_to_local(d.since)
+        d.since = adjust_tz(d.since)
         d._since_def_user = False
 
     return d
